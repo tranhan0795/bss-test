959f81ac3bb79d04aece55085f7b7d110b179b21
Han Tran <tranhan180795@gmail.com>
2025-08-29 10:58:54 +0700

    fix

--- fe.tsx
+++ fe.tsx
@@ -1,32 +1,36 @@
 import react,{useState, useEffect} from "react";
-import { useDebounce } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 const TextSearch = ()=>{
   const [wordNumber, setWordNumber ] = useState();
   const [searchInput, setSeatchInput] = useState("");
-  const [input] = useDebounce(searchInput, 5000);
 
-useEffect(()=>{
-  let shouldUpdate = true;
-     
-   fetch("/api",{body:JSON.stringify({searchInput})}).then(res=>{
+
+const search = (input: string)=>{
+   fetch("/api",{body:JSON.stringify({searchInput: input})}).then(res=>{
      return res.json();
    }).then(n=> {
-    if(shouldUpdate) setWordNumber(n.wordNumber);
+    setWordNumber(n.wordNumber);
    })
-
-   return ()=>{
-    shouldUpdate = false;
-   }
-},[input])
+}
 
   const onChangeInput = (e) =>{
     setSeatchInput(e.target.value);
   }
 
+  const onSearch =()=>{
+     useDebouncedCallback(
+    (value) => {
+      search(value);
+    },
+    5000
+  );
+  }
+
   return <div>
    <p>Result: {wordNumber}</p>
    <input value={searchInput} onChange={onChangeInput}/>
+   <button onClick={onSearch} >search</button>
   </div>
 }
